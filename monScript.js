a06403f56b7e221fb8ad240bcd42a4b4851537dd
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-17 16:43:42 +0100

    change image

--- monScript.js
+++ monScript.js
@@ -1,10 +1,13 @@
 attack = document.getElementById("attack");
 valeurResultat = document.getElementById("valeurResultat");
 afficheAction = document.getElementById("afficheAction");
-attack_dmg =
+perso1 = document.getElementById("perso1");
+
 attack.onclick = function() { 
     valeurResultat.innerHTML = parseInt(valeurResultat.innerHTML)-10;
     afficheAction.innerHTML = "Tu as attaqué et infligé 10 dégats";
-
+    
 }
-
+function attaque(){
+    document.getElementById("image").src="guido.png"
+}
