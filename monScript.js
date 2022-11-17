e331c8c6ca123bb5d019e83c3f3da3cf9b838211
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-17 16:53:40 +0100

    chgmt img

--- monScript.js
+++ monScript.js
@@ -6,8 +6,5 @@ perso1 = document.getElementById("perso1");
 attack.onclick = function() { 
     valeurResultat.innerHTML = parseInt(valeurResultat.innerHTML)-10;
     afficheAction.innerHTML = "Tu as attaqué et infligé 10 dégats";
-    
-}
-function attaque(){
-    document.getElementById("image").src="guido.png"
+    document.getElementById("image").src="guido3.png"
 }
