f6c6db27f3ebb642e418e4db69171612ba27b646
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-25 23:28:11 +0100

    .

--- monScript.js
+++ monScript.js
@@ -1,11 +1,18 @@
 var affichage_PV = document.getElementById("nombre_PV");
 var affichage_PM = document.getElementById("affichage_PM");
+var prc_PV = document.getElementById("prc_PV");
+var prc_PM = document.getElementById("prc_PM");
 var pere_noel = document.getElementById("pere_noel");
-var PRC_perenoel = document.getElementById("prc_PV");
-var PV_perenoel = 1100
+var PV_perenoel = 1100;
+var PM_perenoel = 1100;
 
 pere_noel.onmouseover = function() {
-    affichage_PV.innerHTML =  "PV"
-    PRC_perenoel.value = 100
-    affichage_PM.innerHTML = "PM"
+    affichage_PV.innerHTML = PV_perenoel + "PV"
+    affichage_PM.innerHTML = PM_perenoel + "PM"
+    prc_PV.value = 100
 }
+
+
+
+
+// prc_PV.style.display = "none"
