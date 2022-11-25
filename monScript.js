c10d0e05d0cfad05095f2529a831aba92ad0540b
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-25 15:48:02 +0100

    affichage stats

--- monScript.js
+++ monScript.js
@@ -1,1 +1,11 @@
-var affichage_PV = document.getElementById("affichage_PM")
+var affichage_PV = document.getElementById("nombre_PV");
+var affichage_PM = document.getElementById("affichage_PM");
+var pere_noel = document.getElementById("pere_noel");
+var PRC_perenoel = document.getElementById("prc_PV");
+var PV_perenoel = 1100
+
+pere_noel.onmouseover = function() {
+    affichage_PV.innerHTML =  "PV"
+    PRC_perenoel.value = 100
+    affichage_PM.innerHTML = "PM"
+}
