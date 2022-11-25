526f3cd1275bb7a52615003df9f02d975b1ae980
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-11-26 00:00:02 +0100

    affich pm

--- monScript.js
+++ monScript.js
@@ -1,21 +1,26 @@
 var affichage_PV = document.getElementById("nombre_PV");
-var affichage_PM = document.getElementById("affichage_PM");
+var affichage_PM = document.getElementById("nombre_PM");
 var prc_PV = document.getElementById("prc_PV");
 var prc_PM = document.getElementById("prc_PM");
 var pere_noel = document.getElementById("pere_noel");
 var PV_perenoel = 1100;
 var PM_perenoel = 1100;
 
-pere_noel.onmouseenter = function(test) {
+
+
+
+
+
+pere_noel.onmouseenter = function(){
     affichage_PV.innerHTML = PV_perenoel + "PV"
     affichage_PM.innerHTML = PM_perenoel + "PM"
     prc_PV.value = 100
-    setTimeout(function(){
-        
-    },500)
+    prc_PM.value = 100
 }
 
+pere_noel.onmouseleave =function(leave){
 
+}
 
 
 
