08dbe89681094560ccf0747f50084a227d7539e0
TheoChauveau53 <118454473+TheoChauveau53@users.noreply.github.com>
2022-12-06 09:58:42 +0100

    Update monScript.js

--- monScript.js
+++ monScript.js
@@ -52,81 +52,100 @@ var stats_orque = {
 var tour = 1
 
 // Affichage des stats
-function Affich_stats(perso){
+function Affich_stats(perso,stats_perso){
+    perso.style.filter ="drop-shadow(0 1rem 3rem #ff0000)";
     affichage_stats.style.visibility = "visible";
-    nombre_PV.innerHTML = perso.PV + "PV";
-    nombre_PM.innerHTML = perso.PM + "PM";
-    prc_PV.value = perso.PV;
-    prc_PV.max = perso.PVmax;
-    prc_PM.value = perso.PM;
-    prc_PM.max = perso.PMmax;
+    nombre_PV.innerHTML = stats_perso.PV + "PV";
+    nombre_PM.innerHTML = stats_perso.PM + "PM";
+    prc_PV.value = stats_perso.PV;
+    prc_PV.max = stats_perso.PVmax;
+    prc_PM.value = stats_perso.PM;
+    prc_PM.max = stats_perso.PMmax;
+}
+function Masque_stats(turn,perso){
+    if(tour== turn ){
+        perso.style.filter ="drop-shadow(0 1rem 3rem #0044ff)";
+    }
+    else{
+        perso.style.filter ="none"; 
+    }
+    affichage_stats.style.visibility = "hidden";
 }
 pere_noel.onmouseenter = function(){
-    Affich_stats(stats_perenoel)
+    Affich_stats(pere_noel,stats_perenoel)
 }
 pere_noel.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(1,pere_noel)
 }
 mere_noel.onmouseenter = function(){
-    Affich_stats(stats_merenoel)
+    Affich_stats(mere_noel,stats_merenoel)
 }
 mere_noel.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(2,mere_noel)
 }
 lutin.onmouseenter = function(){
-    Affich_stats(stats_lutin)
+    Affich_stats(lutin,stats_lutin)
 }
 lutin.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(3,lutin)
 }
 cerf.onmouseenter = function(){
-    Affich_stats(stats_cerf)
+    Affich_stats(cerf,stats_cerf)
 }
 cerf.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(4,cerf)
 }
-pingouin.onmouseenter = function Print_pingouin(){
-    Affich_stats(stats_pingouin)
+pingouin.onmouseenter = function(){
+    Affich_stats(pingouin,stats_pingouin)
 }
 pingouin.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(5,pingouin)
 }
 pingouine.onmouseenter = function(){
-    Affich_stats(stats_pingouine)
+    Affich_stats(pingouine,stats_pingouine)
 }
 pingouine.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(6,pingouine)
 }
 orque.onmouseenter = function(){
-    Affich_stats(stats_orque)
+    Affich_stats(orque,stats_orque)
 }
 orque.onmouseleave = function(){
-    affichage_stats.style.visibility = "hidden";
+    Masque_stats(7,orque)
 }
 var victoire=false
-// tour par tour
 
-//    pere_noel.style.filter ="drop-shadow(0 1rem 3rem #0044ff)";
-//    pere_noel.onmouseenter=function(){
-//        pere_noel.style.filter= "drop-shadow(0 1rem 3rem #ff000
-//        affichage_stats.style.visibility = "visible";
-//        nombre_PV.innerHTML = PV_perenoel + "PV";
-//        nombre_PM.innerHTML = PM_perenoel + "PM";
-//        prc_PV.value = PV_perenoel;
-//        prc_PV.max = PVmax_perenoel;
-//        prc_PM.value = PM_perenoel;
-//        prc_PM.max = PMmax_perenoel;
+//function Degats(stats_att,stats_cible){
+//    stats_cible.PV = stats_cible.PV - stats_att.ATK
+//    dialogue.innerHTML = " Vous avez infligé " + stats_att.ATK + "dégats au pingouin";
+//    nombre_PV.innerHTML = stats_cible.PV + "PV";
+//    prc_PV.value = stats_cible.PV;
+//    tour = tour+1;
+//}
+//
+//function Tour(){
+//    if(tour==1){
+//        attack.onclick= function(){
+//            pingouin.onclick(){
+//                Degats(stats_perenoel,stats_pingouin)
+//            }
+//        }   
 //    }
-//    pere_noel.onmouseleave=function(){
-//        pere_noel.style.filter ="drop-shadow(0 1rem 3rem #0044f
-//        affichage_stats.style.visibility = "hidden";
+//}
+
+
+
+// tour par tour
+//while(victoire==false)
+
+    
+//
+//attack.onclick=function(){
+//    pingouin.onclick=function(){           
+//        PV_pingouin = PV_pingouin-ATK_perenoel;
+//        dialogue.innerHTML = " Vous avez infligé " + ATK_perenoel + "dégats au pingouin";
+//        nombre_PV.innerHTML = PV_pingouin + "PV";
+//        prc_PV.value = PV_pingouin;
+//        tour = 2;  
 //    }  
-//    attack.onclick=function(){
-//        pingouin.onclick=function(){           
-//            PV_pingouin = PV_pingouin-ATK_perenoel;
-//            dialogue.innerHTML = " Vous avez infligé " + ATK_perenoel + "dégats au pingouin";
-//            nombre_PV.innerHTML = PV_pingouin + "PV";
-//            prc_PV.value = PV_pingouin;
-//            tour = 2;  
-//        }  
-//    }
+//}
